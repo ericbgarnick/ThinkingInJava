7120db9948132803425c618a28e6c63ff4d2e170
Eric Garnick <eric.b.garnick@gmail.com>
2018-05-06 08:29:53 -0700

    Exercise 1 solved!

---
+++ src/Object_01/DefaultValues.java
@@ -0,0 +1,11 @@
+package Object_01;
+
+public class DefaultValues {
+    static int defaultInt;
+    static char defaultChar;
+
+    public static void main(String[] args) {
+        System.out.println("Default int: " + defaultInt);
+        System.out.println("Default char: " + defaultChar);
+    }
+}
